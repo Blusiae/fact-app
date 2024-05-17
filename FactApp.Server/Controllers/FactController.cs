6eb2ec0c8a6cd268ce051338e582b080945bfc6c
Daniel Kocot <42910231+Blusiae@users.noreply.github.com>
2024-05-17 15:39:04 +0200

    create Get method for facts

---
+++ FactApp.Server/Controllers/FactController.cs
@@ -0,0 +1,23 @@
+﻿using FactApp.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FactApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FactController(HttpClient http) : ControllerBase
+    {
+        [HttpGet("/fact")]
+        public async Task<ActionResult<FactDto>> GetFact()
+        {
+            var response = await http.GetAsync("https://catfact.ninja/fact");
+            var fact = await response.Content.ReadFromJsonAsync<FactDto>();
+
+            if(fact is null)
+                return NotFound();
+
+            return Ok(fact);
+        }
+    }
+}
